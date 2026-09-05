b14f035f7461c2da636a85ef9b34ce05016a57df
Mailen Claros <129794591+MailenClaros15@users.noreply.github.com>
2026-09-05 19:10:52 -0300

    Agrega configuracion de JPA, build.gradle, Usuario y EntityId

---
+++ 8- ORM – JPA - Hibernate/8-ORM-JPA-Hibernate/src/main/java/com/facturaarca/entidades/EntityId.java
@@ -0,0 +1,25 @@
+package com.facturaarca.entidades;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class EntityId {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    public EntityId() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
